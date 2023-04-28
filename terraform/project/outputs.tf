d3409645f453a16d891a5e6df16862abb315b87c
George Aristy <george.aristy@gmail.com>
2023-04-28 11:18:58 -0400

    tf: got rid of CD for now

--- terraform/project/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "project_id" {
-  description = "Project ID"
-  value       = module.project.project_id
-}
-
-output "site_storage_bucket_name" {
-  description = "Name of GCS bucket where site is hosted."
-  value       = google_storage_bucket.my-site.name
-}
